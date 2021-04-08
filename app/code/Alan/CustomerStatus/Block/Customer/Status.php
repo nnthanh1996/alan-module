58eb3b92c89f4a456004d74e0370428a33fd6183
Thanh Nguyen <nnthanh1996@gmail.com>
2021-04-08 23:16:34 +0700

    Finish module

--- app/code/Alan/CustomerStatus/Block/Customer/Status.php
+++ app/code/Alan/CustomerStatus/Block/Customer/Status.php
@@ -9,4 +9,30 @@ use Magento\Framework\View\Element\Template;
 class Status extends Template
 {
 
+    /** @var \Magento\Customer\Model\Session $customerSession */
+    protected $customerSession;
+
+    public function __construct(
+        Template\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+        $this->customerSession = $customerSession;
+    }
+
+    public function getCustomerStatus() {
+
+        if($this->customerSession->isLoggedIn()) {
+
+            $customer = $this->customerSession->getCustomer();
+
+            return $customer->getData('customer_status');
+
+        }
+
+        return null;
+    }
+
 }
